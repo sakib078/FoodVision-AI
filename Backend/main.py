b9cd8473ee3448c9333654a4f937a4427a0de425
sakib078 <sakibmansuri044@gmail.com>
2026-04-17 21:47:34 -0400

    production mono-repo frontend

--- Backend/main.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.config import DEFAULT_HOST, DEFAULT_PORT
-
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(host=DEFAULT_HOST, port=DEFAULT_PORT, debug=False)
